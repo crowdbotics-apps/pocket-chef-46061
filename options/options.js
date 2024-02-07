00469914f40b4bd464ab97947921a88fa1c0488c
Crowdbotics <team@crowdbotics.com>
2024-02-07 15:59:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pocket-chef-46061",
+  url: "https://pocket-chef-46061.botics.co",
+  api: "https://pocket-chef-46061.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
